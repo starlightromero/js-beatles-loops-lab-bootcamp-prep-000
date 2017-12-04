352643a538259568c1bfc0158c58ecbf6c03a65a
Starlight Romero <starlightromero@gmail.com>
2017-12-04 18:28:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var array = []
   while (array.length < facts.length ) {
-    array.push(facts + '!!!')
+    array.push(facts + "!!!"")
   }
   return array
 }
