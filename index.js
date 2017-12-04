fa6ca945b94f92118dc43f15c5be64d93a952ae7
Starlight Romero <starlightromero@gmail.com>
2017-12-04 18:10:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (i = 0; i > 3; i++) {
+  for (i = 0; i < 3; i++) {
     array.push(musicians + ' plays ' + instruments)
   }
   return array
